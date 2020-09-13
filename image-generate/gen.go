46a89aaae8ef5e2e7dee78ef74e6353b66a84551
Dami <obayanjud@gmail.com>
2020-09-13 14:22:53 -0600

    Unmarshal bytes response to JSON

--- image-generate/gen.go
+++ image-generate/gen.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -29,17 +30,26 @@ func getDataFromURL(url string) []byte {
 	return body
 }
 
+func unmarshalJSON(byt []byte) map[string]interface{} {
+	var dat map[string]interface{}
+	if err := json.Unmarshal(byt, &dat); err != nil {
+		log.Fatal(err)
+	}
+	return dat
+}
+
 func getImage(category, downloadDir string) error {
 	param := url.Values{}
 	param.Set("key", API_KEY)
 	param.Set("category", category)
+	param.Set("per_page", "3")
 	queryString := param.Encode()
 
 	url := fmt.Sprintf("%s?%s", PixabayURL, queryString)
 	fmt.Println(url)
 	byt := getDataFromURL(url)
-	fmt.Println(byt)
 
+	jsonData := unmarshalJSON(byt)
 	return nil
 }
 
